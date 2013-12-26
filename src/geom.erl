587190902b863645a84bc4181d2d22528d845464
mpm <mpmiszczyk@gmail.com>
2013-12-27 00:09:50 +0100

    Etude 3-4: changed function with arity to tuple.

--- src/geom.erl
+++ src/geom.erl
@@ -2,22 +2,22 @@
 
 
 -export( [area/2,
-          area/3]).
+          area/1]).
 
 
 area( Lenght, Height ) ->
   Lenght * Height.
 
-area( rectangle, Lenght, Height ) when Lenght >= 0 andalso Height >= 0 ->
+area( {rectangle, Lenght, Height}) when Lenght >= 0 andalso Height >= 0 ->
   area( Lenght, Height );
 
-area( triangle, Lenght, Height ) when Lenght >= 0 andalso Height >= 0->
+area( {triangle, Lenght, Height}) when Lenght >= 0 andalso Height >= 0->
   Lenght * Height / 2;
 
-area( ellipse, Lenght, Height) when Lenght >= 0 andalso Height >= 0  ->
+area( {ellipse, Lenght, Height}) when Lenght >= 0 andalso Height >= 0  ->
   math:pi() * Lenght * Height;
 
-area( _UnknownShape, Lenght, Height) when Lenght >= 0 andalso Height >= 0 ->
+area( {_UnknownShape, Lenght, Height}) when Lenght >= 0 andalso Height >= 0 ->
   0.
 
 
--- test/e03_test.erl
+++ test/e03_test.erl
@@ -7,22 +7,22 @@
 
 should_calculate_area_of_rectangle_by_explicit_atom_test() ->
   ?assertEqual( 12,
-                geom:area( rectangle, 3, 4)).
+                geom:area( {rectangle, 3, 4})).
 
 
 should_calculate_area_of_triangle__test() ->
   ?assertEqual( 7.5,
-                geom:area( triangle, 3, 5)).
+                geom:area( {triangle, 3, 5})).
 
 should_calculate_area_of_ellipse_by_explicit_atom_test() ->
   ?assertEqual( 25.132741228718345,
-                geom:area( ellipse, 2, 4)).
+                geom:area( {ellipse, 2, 4})).
 
 should_throw_an_error_when_calling_fun_with_negative_numbers_test() ->
   ?assertError( function_clause,
-                geom:area( rectangle, -1, 0)).
+                geom:area( {rectangle, -1, 0})).
 
 should_return_zero_on_any_unknown_shape_test() ->
   ?assertEqual( 0,
-                geom:area( some_unknown_shape, 100, 99)).
+                geom:area( {some_unknown_shape, 100, 99})).
   
