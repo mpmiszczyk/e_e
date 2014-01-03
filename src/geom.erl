3cff20a16ed3055db54c475483e7f64e95d2d87d
mpm <mpmiszczyk@gmail.com>
2014-01-03 22:42:29 +0100

    Bit of haskel alighning.

--- src/geom.erl
+++ src/geom.erl
@@ -10,10 +10,10 @@ area( Lenght, Height ) ->
 
 area( {Figure, Lenght, Height}) when Lenght >= 0 andalso Height >= 0 ->
   case Figure of
-    rectangle -> Lenght * Height;
-    triangle -> Lenght * Height /2;
-    elliprs ->   math:pi() * Lenght * Height;
-    _Unknown -> 0
+    rectangle  -> Lenght * Height;
+    triangle   -> Lenght * Height /2;
+    elliprs    ->   math:pi() * Lenght * Height;
+    _Unknown   -> 0
   end.
 
 
