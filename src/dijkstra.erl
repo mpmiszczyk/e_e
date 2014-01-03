377569e064c59eae822fef9182fff958092925e5
mpm <mpmiszczyk@gmail.com>
2014-01-03 23:40:14 +0100

    Additional tests and proper algorithm implementation.

--- src/dijkstra.erl
+++ src/dijkstra.erl
@@ -3,5 +3,11 @@
 -export([gcd/2]).
 
 
-gcd(12,8) ->
-  4.
+gcd( A, B)  when A =:= B ->
+  A;
+gcd( A, B) when A > B ->
+  gcd( A-B, B);
+gcd( A, B) when A < B ->
+  gcd( A, B-A).
+
+
--- test/e04_test.erl
+++ test/e04_test.erl
@@ -3,4 +3,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 basic_dijkstra_gcd_test() ->
-  ?assertEqual( 4, dijkstra:gcd(12,8)).
+  ?assertEqual( 4, dijkstra:gcd( 12, 8 )),
+  ?assertEqual( 7, dijkstra:gcd( 14, 21)),
+  ?assertEqual( 1, dijkstra:gcd(125, 46)),
+  ?assertEqual(12, dijkstra:gcd(120, 36)).
